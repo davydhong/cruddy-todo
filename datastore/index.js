4d3607ecf66a5d16ea467fbace361c8c77647430
davydhong <davydhong@gmail.com>
2018-08-23 18:23:06 -0700

    refactored readAll

--- datastore/index.js
+++ datastore/index.js
@@ -2,15 +2,17 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
-
 var items = {};
+const Promise = require('bluebird');
+
+var readFilePromisified = Promise.promisify(fs.readFile);
 
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
   counter.getNextUniqueId((err, id) => {
-		//NOTE: 1st Arg takes an file/file path. Also passing (err) => ... per documentation.
-    var targetId = exports.dataDir + `/${id}.txt`;
+    //NOTE: 1st Arg takes an file/file path. Also passing (err) => ... per documentation.
+    var targetId = path.join(exports.dataDir, `/${id}.txt`);
     fs.writeFile(targetId, text, err => {
       if (err) {
         throw new Error('Create not run. Review text and callback inputs');
@@ -21,7 +23,7 @@ exports.create = (text, callback) => {
 };
 
 exports.readOne = (id, callback) => {
-  var targetId = exports.dataDir + `/${id}.txt`;
+  var targetId = path.join(exports.dataDir, `/${id}.txt`);
   fs.readFile(targetId, (err, data) => {
     if (err) {
       callback(new Error(`No item with id: ${id}`));
@@ -33,28 +35,34 @@ exports.readOne = (id, callback) => {
 
 exports.readAll = callback => {
   fs.readdir(exports.dataDir, (err, files) => {
-    var data = [];
+    // var data = [];
     if (err) {
       throw new Error('readAll not running (i.e. directory not found)');
     }
-    _.each(files, (file, idx) => {
-			// _.each loops through the folder and pushes to the 'data' array.
-			// using .slice(0,-4) to cut off '.txt' from file names.
-      data.push({ id: files[idx].slice(0, -4), text: files[idx].slice(0, -4) });
+    var data = _.map(files, file => {
+      console.log(files, file);
+      var id = path.basename(file, '.txt');
+      var filePath = path.join(exports.dataDir, file);
+      return readFilePromisified(filePath).then(text => {
+        return { id: id, text: text.toString() };
+      });
     });
-    callback(null, data);
+    console.log(data);
+    Promise.all(data).then(obj => {
+      callback(null, obj);
+    });
   });
 };
 
 exports.update = (id, text, callback) => {
-  var targetId = exports.dataDir + `/${id}.txt`;
+  var targetId = path.join(exports.dataDir, `/${id}.txt`);
   fs.readFile(targetId, (err, data) => {
     if (err) {
-			//NOTE: if readfile can't find the target file, return error
-			// if the file is found, exercise fs.writefile
+      //NOTE: if readfile can't find the target file, return error
+      // if the file is found, exercise fs.writefile
       callback(new Error(`No item with id: ${id}`));
     } else {
-			//NOTE!!: else state above is required to prevent non-existent id from trickling down to fs.writefile.
+      //NOTE!!: else state above is required to prevent non-existent id from trickling down to fs.writefile.
       fs.writeFile(targetId, text, err => {
         if (err) {
           callback(new Error(`No item with id: ${id}`));
@@ -69,7 +77,7 @@ exports.update = (id, text, callback) => {
 exports.delete = (id, callback) => {
   fs.unlink(exports.dataDir + `/${id}.txt`, err => {
     if (err) {
-			// report an error if item not found
+      // report an error if item not found
       callback(new Error(`No item with id: ${id}`));
     } else {
       callback();
